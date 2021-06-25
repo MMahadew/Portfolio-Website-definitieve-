3ad9c72d1c127a85259d82d8d9f655f97bef3c76
MMahadew <70715422+MMahadew@users.noreply.github.com>
2021-06-25 12:36:28 +0200

    Add files via upload

--- Javascript/Javascript2.js
+++ Javascript/Javascript2.js
@@ -12,12 +12,12 @@ if (currentTheme == "dark") {
 btn.addEventListener("click", function () {
   if (prefersDarkScheme.matches) {
     document.body.classList.toggle("light-theme");
-    var theme = document.body.classList.contains("light-theme")
+     theme = document.body.classList.contains("light-theme")
       ? "light"
       : "dark";
   } else {
     document.body.classList.toggle("dark-theme");
-    var theme = document.body.classList.contains("dark-theme")
+     theme = document.body.classList.contains("dark-theme")
       ? "dark"
       : "light";
   }
